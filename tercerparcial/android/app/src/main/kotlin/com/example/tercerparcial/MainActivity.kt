145fccdfda629c875e8a75562b1c316b4ac8975a
Eduardo199614 <77286190+Eduardo199614@users.noreply.github.com>
2021-08-03 10:41:32 -0500

    tercerparcial

---
+++ tercerparcial/android/app/src/main/kotlin/com/example/tercerparcial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tercerparcial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tercerparcial/lib/main.dart
@@ -0,0 +1,24 @@
+  
+import 'package:flutter/material.dart';
+
+import 'navbar.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tres Pantallas',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NavBar(),
+    );
+  }
+}
